72c16a41b04949483a2bab2647e24724f823b92e
vmxplus <vmxplus.cs@gmail.com>
2015-05-29 11:30:20 +0800

    The commit test

--- src/sed/algorithm/chapter1/section5/HelloWorld.java
+++ src/sed/algorithm/chapter1/section5/HelloWorld.java
@@ -2,6 +2,6 @@ package sed.algorithm.chapter1.section5;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("This is  a test");
+		System.out.println("This is   est");
 	}
 }
